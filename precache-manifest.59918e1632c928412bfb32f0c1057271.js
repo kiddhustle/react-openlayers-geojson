b6c6e5530dfeec9df27b2ca7e225f96afb2a96b6
kiddhustle <kiddhustle@wiardweb.com>
2020-06-08 18:13:14 +0100

    Updates

---
+++ precache-manifest.59918e1632c928412bfb32f0c1057271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435e4634b3e5fd18b630490b872ab503",
+    "url": "/react-openlayers-geojson/index.html"
+  },
+  {
+    "revision": "cf1c238093240d11a37a",
+    "url": "/react-openlayers-geojson/static/css/2.db529fa1.chunk.css"
+  },
+  {
+    "revision": "7bb60be778119909343f",
+    "url": "/react-openlayers-geojson/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cf1c238093240d11a37a",
+    "url": "/react-openlayers-geojson/static/js/2.dac2810b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-openlayers-geojson/static/js/2.dac2810b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bb60be778119909343f",
+    "url": "/react-openlayers-geojson/static/js/main.b62a9fce.chunk.js"
+  },
+  {
+    "revision": "8dce2c3e2dac1f6b630d",
+    "url": "/react-openlayers-geojson/static/js/runtime-main.62f40f1e.js"
+  }
+]);
